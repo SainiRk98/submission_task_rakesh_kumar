b95063e921e13206f190a177cde9a43ea1f000ac
Rakesh Kumar <rakesh.10april.1998@gmail.com>
2026-08-11 15:12:45 +0530

    Add files via upload

---
+++ permutation.py
@@ -0,0 +1,12 @@
+def permutation(nums):
+    n=len(nums)
+    while i > 0:
+        i -= 1
+        if i >=0:
+            left=i+1
+            right=n-1
+    while left< right:
+        left +=1
+        right -=1
+nums = [3, 2, 1]
+print(permutation(nums))
